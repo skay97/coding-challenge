3a9bd0f76260ca75b4b137c031dcaa24ea4fcc43
Salman Karim <Lego@macbook-pro-2.home>
2019-02-19 01:09:40 -0600

    update

--- Assets/JavaScript/test.js
+++ Assets/JavaScript/test.js
@@ -7,7 +7,8 @@ function rand11() {
 function rand13() {
     // var newRandom stores numbers from a range of 1-121
     var newRandom = 11 * rand11() + rand11() - 11;
-    // If the number generated is less than 118, the number will be divded by 13 and its remainder will be added by 1 to provide a final range of 1-13. Otherwise, a new number is generated and is run through the process again
+    // If the number generated is less than 118, the number will be divided by 13 and its remainder will be added by 1 to provide a final range of 1-13. 
+    // Otherwise, a new number is generated and is run through the process again
     return newRandom < 118 ? (newRandom % 13) + 1 : rand13();
 }
 // rand13 function is called to present final output
